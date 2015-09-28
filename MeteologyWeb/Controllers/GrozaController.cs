a23e41533496da65ee97c65a8734e7f87822b3fd
Mikhail Gerasimov <mgerasim@inbox.ru>
2015-09-28 04:13:52 +0000

    add-chart

--- MeteologyWeb/Controllers/GrozaController.cs
+++ MeteologyWeb/Controllers/GrozaController.cs
@@ -47,6 +47,57 @@ namespace MeteologyWeb.Controllers
             ViewBag.SeriesLatMin = (int)(theGrozes.Min(x => x.Latitude) * 10);
             ViewBag.CountCountrolLat = countSeriesLat;
 
+
+
+            string theSeriesSumLongMinus = "";
+            string theSeriesSumLongPlus = "";
+            int countSeriesSumLongPlus = 0;
+            int countSeriesSumLongMinus = 0;
+            for (int i = (int)(theGrozes.Min(x => x.Longitude) * 10); i < (int)(theGrozes.Max(x => x.Longitude) * 10); i++)
+            {
+                int countPlus = theGrozes.Where(x => (int)(x.Longitude * 10) == i).Where(x => x.Intensity > 0).Sum(x => x.Intensity);
+                int countMinus = theGrozes.Where(x => (int)(x.Longitude * 10) == i).Where(x => x.Intensity < 0).Sum(x => x.Intensity);
+                countSeriesSumLongPlus += countPlus;
+                countSeriesSumLongMinus += countMinus;
+                theSeriesSumLongPlus += countPlus.ToString() + ",";
+                theSeriesSumLongMinus += countMinus.ToString() + ",";
+            }
+            theSeriesSumLongPlus = theSeriesSumLongPlus.TrimEnd(trims);
+            theSeriesSumLongMinus = theSeriesSumLongMinus.TrimEnd(trims);
+            theSeriesSumLongPlus = "[" + theSeriesSumLongPlus + "]";
+            theSeriesSumLongMinus = "[" + theSeriesSumLongMinus + "]";
+            ViewBag.SeriesSumLongPlus = theSeriesSumLongPlus;
+            ViewBag.SeriesSumLongMinus = theSeriesSumLongMinus;
+            ViewBag.SeriesSumLongMin = (int)(theGrozes.Min(x => x.Latitude) * 10);
+            ViewBag.ControlSeriesSumLong = countSeriesSumLongPlus;
+
+
+
+
+            string theSeriesSumLatMinus = "";
+            string theSeriesSumLatPlus = "";
+            int countSeriesSumLatPlus = 0;
+            int countSeriesSumLatMinus = 0;
+            for (int i = (int)(theGrozes.Min(x => x.Latitude) * 10); i < (int)(theGrozes.Max(x => x.Latitude) * 10); i++)
+            {
+                int countPlus = theGrozes.Where(x => (int)(x.Latitude * 10) == i).Where(x => x.Intensity > 0).Sum(x => x.Intensity);
+                int countMinus = theGrozes.Where(x => (int)(x.Latitude * 10) == i).Where(x => x.Intensity < 0).Sum(x => x.Intensity);
+                countSeriesSumLatPlus += countPlus;
+                countSeriesSumLatMinus += countMinus;
+                theSeriesSumLatPlus += countPlus.ToString() + ",";
+                theSeriesSumLatMinus += countMinus.ToString() + ",";
+            }
+            theSeriesSumLatPlus = theSeriesSumLatPlus.TrimEnd(trims);
+            theSeriesSumLatMinus = theSeriesSumLatMinus.TrimEnd(trims);
+            theSeriesSumLatPlus = "[" + theSeriesSumLatPlus + "]";
+            theSeriesSumLatMinus = "[" + theSeriesSumLatMinus + "]";
+            ViewBag.SeriesSumLatPlus = theSeriesSumLatPlus;
+            ViewBag.SeriesSumLatMinus = theSeriesSumLatMinus;
+            ViewBag.SeriesSumLatMin = (int)(theGrozes.Min(x => x.Latitude) * 10);
+            ViewBag.ControlSeriesSumLat = countSeriesSumLatPlus;
+
+
+
             return View(theGrozes);
         }
 
